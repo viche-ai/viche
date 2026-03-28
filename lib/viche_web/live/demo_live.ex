c1179b939247562b38e950941da9ccc70c189f46
Joel Milligan <joeldevelops@gmail.com>
2026-03-28 15:59:32 +0100

    demo: show live agent count instead of fake join counter

--- lib/viche_web/live/demo_live.ex
+++ lib/viche_web/live/demo_live.ex
@@ -6,36 +6,17 @@ defmodule VicheWeb.DemoLive do
     agents = Viche.Agents.list_agents()
 
     if connected?(socket) do
-      Phoenix.PubSub.subscribe(Viche.PubSub, "demo:joins")
       Phoenix.PubSub.subscribe(Viche.PubSub, "registry:global")
-      Process.send_after(self(), :fake_join, :rand.uniform(5000) + 5000)
     end
 
     {:ok,
      assign(socket,
-       join_count: 0,
-       qr_hash: "a8f3c2",
        agent_count: length(agents),
        messages_today: 0
      )}
   end
 
   @impl true
-  def handle_info(:fake_join, socket) do
-    count = socket.assigns.join_count
-
-    if count < 50 do
-      Process.send_after(self(), :fake_join, :rand.uniform(5000) + 4000)
-      {:noreply, assign(socket, join_count: count + 1)}
-    else
-      {:noreply, socket}
-    end
-  end
-
-  def handle_info(%Phoenix.Socket.Broadcast{event: "new_join"}, socket) do
-    {:noreply, assign(socket, join_count: socket.assigns.join_count + 1)}
-  end
-
   def handle_info(%Phoenix.Socket.Broadcast{event: "agent_joined"}, socket) do
     {:noreply, assign(socket, agent_count: length(Viche.Agents.list_agents()))}
   end
